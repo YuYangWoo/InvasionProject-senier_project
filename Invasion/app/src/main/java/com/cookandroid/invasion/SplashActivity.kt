3efce145987d773c484b7ec645a5ea2d35c7791a
yuyw0712 <yuyw0712@naver.com>
2020-09-14 19:35:16 +0900

    Splash 화면 연결

---
+++ Invasion/app/src/main/java/com/cookandroid/invasion/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.cookandroid.invasion
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity(){
+
+    private val splashTime:Long = 1500
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_activity)
+
+        // 2초 지나고 화면 전환
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        },splashTime)
+    }
+}
