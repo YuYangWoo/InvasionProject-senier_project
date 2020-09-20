44e29254a26d852a30a68120f29c71b411e1dc6e
yuyw0712 <yuyw0712@naver.com>
2020-09-20 22:06:11 +0900

    코드 리팩토링

--- Invasion/app/src/main/java/com/cookandroid/invasion/Log/LogActivity.kt
+++ Invasion/app/src/main/java/com/cookandroid/invasion/Log/LogActivity.kt
@@ -1,21 +1,35 @@
 package com.cookandroid.invasion.Log
 
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import com.cookandroid.invasion.R
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import kotlinx.android.synthetic.main.activity_log.*
 
 class LogActivity : AppCompatActivity() {
-
+    var abc = R.drawable.splashscreen
     var logList = arrayListOf<LogItem>(
-        LogItem("도둑이 침입했습니다!", "2020.09.16 18:00", "splashscreen"),
-        LogItem("도둑이 침입했습니다!", "2020.09.16 18:00", "splashscreen"),
-        LogItem("도둑이 침입했습니다!", "2020.09.16 18:00", "splashscreen"),
-        LogItem("도둑이 침입했습니다!", "2020.09.16 18:00", "splashscreen"),
-        LogItem("도둑이 침입했습니다!", "2020.09.16 18:00", "splashscreen")
+        LogItem("fasd", "Fsdfsa", abc.toString()),
+        LogItem("fasd", "Fsdfsa", abc.toString()),
+        LogItem("fasd", "Fsdfsa", abc.toString())
     )
+
+    var storage = Firebase.storage("gs://cerberus-8f761.appspot.com")
+
+    // Create a storage reference from our app
+    val storageRef = storage.reference
+
+    // 하위 위치를 가리키는 참조
+
+    val spaceRef = storageRef.child("cerb1/cue.jpg")
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log)
@@ -23,9 +37,14 @@ class LogActivity : AppCompatActivity() {
         // ActionBar Title 변경
         supportActionBar?.title = "알림"
 
+        Log.d("test", logList.toString())
+
         // ActionBar Home 버튼 Enable
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        Glide.with(this)
+            .load(spaceRef)
+            .into(img)
         // LogAdapter 객체 생성 후 recyclerView와 연동
         val logAdapter = LogAdapter(this, logList)
         recyclerView.adapter = logAdapter
