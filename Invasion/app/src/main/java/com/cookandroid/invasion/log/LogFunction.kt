9f181d0071a998f26750872f9db96032c24bc999
yuyw0712 <yuyw0712@naver.com>
2020-09-26 17:30:33 +0900

    recyclerView 클릭 후 액티비티 기본 레이아웃 생성

---
+++ Invasion/app/src/main/java/com/cookandroid/invasion/log/LogFunction.kt
@@ -0,0 +1,12 @@
+package com.cookandroid.invasion.log
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cookandroid.invasion.R
+
+class LogFunction : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_function)
+    }
+}
