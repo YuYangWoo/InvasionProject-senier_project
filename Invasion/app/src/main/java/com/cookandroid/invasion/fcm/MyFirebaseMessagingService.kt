46a224f59b58c845adf9a1bdcd4388189888b09b
yuyw0712 <yuyw0712@naver.com>
2020-10-30 18:29:52 +0900

    FCM head up notification 구현

--- Invasion/app/src/main/java/com/cookandroid/invasion/fcm/MyFirebaseMessagingService.kt
+++ Invasion/app/src/main/java/com/cookandroid/invasion/fcm/MyFirebaseMessagingService.kt
@@ -1,13 +1,16 @@
 package com.cookandroid.invasion.fcm
 
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.media.RingtoneManager
+import android.os.Build
 import android.provider.Settings
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.cookandroid.invasion.MainActivity
 import com.cookandroid.invasion.R
 import com.google.firebase.database.DatabaseReference
@@ -35,6 +38,12 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     // 새로운 FCM 메시지가 있을 때 메세지를 받는다
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         Log.d(TAG, "From: " + remoteMessage.from)
+
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d(TAG,"빠끄빠끄")
+            // 사용자에게 먼저 메세지를 보여줍니다.
+            sendNotification(remoteMessage.notification?.body)
+        }
         // 앱이 포어그라운드 상태에서 Notificiation을 받는 경우
         if(remoteMessage.notification != null) {
             Log.d(TAG, "Notification Message Body: ${remoteMessage.notification?.body}")
@@ -55,16 +64,44 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         var pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
         val notificationSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
 
-        var notificationBuilder = NotificationCompat.Builder(this,"Notification")
+        // head up
+        val NOTIFICATION_ID = 1001
+        createNotificationChannel(this, NotificationManagerCompat.IMPORTANCE_HIGH, false,
+            getString(R.string.app_name), "App notification channel")   // 1
+
+        val channelId = "$packageName-${getString(R.string.app_name)}"
+
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        val fullScreenPendingIntent = PendingIntent.getActivity(baseContext, 0,
+            intent, PendingIntent.FLAG_UPDATE_CURRENT)    // 2
+
+        var notificationBuilder = NotificationCompat.Builder(this,channelId)
             .setSmallIcon(R.mipmap.ic_launcher)
-            .setContentTitle("Push Notification FCM")
+            .setContentTitle("알림")
             .setContentText(body)
             .setAutoCancel(true)
             .setSound(notificationSound)
             .setContentIntent(pendingIntent)
+            .setFullScreenIntent(fullScreenPendingIntent, true)
+            .setTimeoutAfter(1500)
 
-        var notificationManager: NotificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.notify(0, notificationBuilder.build())
+        notificationBuilder.priority = NotificationCompat.PRIORITY_HIGH
+//        var notificationManager: NotificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationManager = NotificationManagerCompat.from(this)
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
+    }
+
+    private fun createNotificationChannel(context: Context, importance: Int, showBadge: Boolean,
+                                          name: String, description: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "${context.packageName}-$name"
+            val channel = NotificationChannel(channelId, name, importance)
+            channel.description = description
+            channel.setShowBadge(showBadge)
+
+            val notificationManager = context.getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
 }
