e6f556ea38d6f8893bf92817d756bfe80af3b24c
yuyw0712 <yuyw0712@naver.com>
2020-11-01 03:35:20 +0900

    Foreground, Background, Killed 상태에서 FCM 기능 완전 안정화

--- Invasion/app/src/main/java/com/cookandroid/invasion/fcm/MyFirebaseMessagingService.kt
+++ Invasion/app/src/main/java/com/cookandroid/invasion/fcm/MyFirebaseMessagingService.kt
@@ -31,53 +31,47 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         //파베 연결해서 토큰 날려줘야함. 단 한번만 날린다.
         database = FirebaseDatabase.getInstance()
         databaseReference = database.getReference("Token")
-        val idByANDROID_ID: String = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID)
-        databaseReference.child(idByANDROID_ID).setValue(token);
+        val idByANDROID: String = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID)
+        databaseReference.child(idByANDROID).setValue(token);
     }
-    
+
     // 새로운 FCM 메시지가 있을 때 메세지를 받는다
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        Log.d(TAG, "From: " + remoteMessage.from)
-
-        if (remoteMessage.data.isNotEmpty()) {
-            Log.d(TAG,"빠끄빠끄")
-            // 사용자에게 먼저 메세지를 보여줍니다.
-            sendNotification(remoteMessage.notification?.body)
-        }
         // 앱이 포어그라운드 상태에서 Notificiation을 받는 경우
         if(remoteMessage.notification != null) {
-            Log.d(TAG, "Notification Message Body: ${remoteMessage.notification?.body}")
-            sendNotification(remoteMessage.notification?.body)
+            sendNotification(remoteMessage.notification?.body, remoteMessage.notification?.title)
         }
         else {
-            sendNotification(remoteMessage.notification?.body)
+            sendNotification(remoteMessage.notification?.body, remoteMessage.notification?.title)
         }
     }
 
     // FCM 메시지를 보내는 메시지
-    private fun sendNotification(body: String?) {
+    private fun sendNotification(body: String?, title: String?) {
         val intent = Intent(this, MainActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
             putExtra("Notification", body)
+            putExtra("Notification",title)
         }
 
         var pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
         val notificationSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
 
-        // head up
-        val NOTIFICATION_ID = 1001
+        // head up 알림 생성하기
+        val notificationId = 1001
         createNotificationChannel(this, NotificationManagerCompat.IMPORTANCE_HIGH, false,
-            getString(R.string.app_name), "App notification channel")   // 1
+            getString(R.string.app_name), "App notification channel") 
 
         val channelId = "$packageName-${getString(R.string.app_name)}"
 
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         val fullScreenPendingIntent = PendingIntent.getActivity(baseContext, 0,
-            intent, PendingIntent.FLAG_UPDATE_CURRENT)    // 2
+            intent, PendingIntent.FLAG_UPDATE_CURRENT)  
 
+        // 푸시알람 부가설정
         var notificationBuilder = NotificationCompat.Builder(this,channelId)
             .setSmallIcon(R.mipmap.ic_launcher)
-            .setContentTitle("알림")
+            .setContentTitle(title)
             .setContentText(body)
             .setAutoCancel(true)
             .setSound(notificationSound)
@@ -86,11 +80,11 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
             .setTimeoutAfter(1500)
 
         notificationBuilder.priority = NotificationCompat.PRIORITY_HIGH
-//        var notificationManager: NotificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        val notificationManager = NotificationManagerCompat.from(this)
-        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
+        var notificationManager: NotificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(notificationId, notificationBuilder.build())
     }
 
+    // NotificationChannel 만드는 메서드
     private fun createNotificationChannel(context: Context, importance: Int, showBadge: Boolean,
                                           name: String, description: String) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -103,6 +97,5 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
             notificationManager.createNotificationChannel(channel)
         }
     }
-
 }
 
---
+++ fcm.js
@@ -0,0 +1,123 @@
+const firebase = require('firebase');
+var firebaseConfig = {
+  apiKey: "AIzaSyAdoflUdlFVW4o7tB_xQ9JAsBmABJ8BMpk",
+  authDomain: "cerberus-592f9.firebaseapp.com",
+  databaseURL: "https://cerberus-592f9.firebaseio.com",
+  projectId: "cerberus-592f9",
+  storageBucket: "cerberus-592f9.appspot.com",
+  messagingSenderId: "213036694754",
+  appId: "1:213036694754:web:239615e2bb106259b072bb",
+  measurementId: "G-Q0HFH3Y4QG"
+};
+firebase.initializeApp(firebaseConfig);
+const request = require('request');
+
+const options = {
+    uri:'https://fcm.googleapis.com/fcm/send', 
+    method: 'POST',
+    headers: {
+        "content-type": "application/json",
+        "Authorization": "key=AAAAMZn5_OI:APA91bEa17m4brjjK48PdNJy6GIGWSC57G89fLU9dzWLNK4bZmPvdC1eHwcpYugch_2IMfunGbV6kjhnTJJt6S4ChO4r-ij0QAWjH1MEHxk-_0Pn__PzEjlE5xC4SiV5BEOjCubliZ25"
+    },
+    json: {
+        'to': "fGKtMPS-XFg:APA91bFdHJOAOEMYixKCs1IvbJI1wa73xDQ4HFXhNuKyNUuI03kGieNxLF3BpP3ur4lV9zR44sOr2KfNqHD3unCzrlWcOVqVV-JiTCGOKtPL7YouPPrLt0QzMGe3IkJnRgfqPEcxQSl9",
+        'notification': {
+            'body': '',
+            'title': '',
+            'key_1': ''
+        },
+        'data': {
+          'body': '',
+          'title': '',
+          'key_1': ''
+        },
+        "message": {
+          "data" : {
+             "title" : "",
+             "body"  : ""
+          }
+        }
+    }
+  }
+//request.post(options, function(err,httpResponse,body){ /* ... */ })
+
+// 파베 접근해서 token을 받아온다.
+var key=[];
+var token=[];
+// 파베 접근해서 token을 받아온다.
+firebase.database().ref("Token").on('value', (snapshot)=>{
+  //console.log(snapshot.val());
+  val = snapshot.val();
+  console.log(val);
+  //키값들을 받는다.
+  key = Object.keys(val);
+  // 토큰값을 받는다.
+  token = Object.values(val);
+  console.log(key);
+  console.log(token);
+})
+
+
+User = {
+  id:"cerberusTable",
+  door: 0,
+  door_open: 0,
+  danger: 0,
+  alarm:0
+};
+//데이터 쓰기
+//firebase.database().ref('cerb1/door').set(1);
+
+// firebase를 통해서 값 실시간 모니터링
+firebase.database().ref(User.id+"/door").on('value', (snapshot)=>{
+    //console.log(snapshot.val());
+      val = snapshot.val();
+      if(val == 1){//door opened
+        //서버에 알려주기
+        User.door = val;
+        console.log("door opend!!")
+        options.json.notification.title = "문열림 알림"
+        options.json.notification.body = "현관문이 열렸습니다."
+      }else{
+        User.door = val;
+        console.log("door closed");
+        options.json.notification.body = "현관문이 닫혔습니다."
+        options.json.notification.title = "문닫힘 알림"
+      }
+      token.forEach(function(element){
+        options.json.to = element;
+        request.post(options, function(err,httpResponse,body){ /* ... */ });
+      })
+})
+firebase.database().ref(User.id+"/danger").on('value', (snapshot)=>{
+  //console.log(snapshot.val());
+    val = snapshot.val();
+    if(val == 1){//danger!!
+      User.danger = val;
+      console.log("Danger!!!!!!")
+      options.json.notification.title = "위험수준 : 높음"
+      options.json.notification.body = "신발을 벗지 않은 내부에 침입자가 있습니다."
+      token.forEach(function(element){
+        options.json.to = element;
+        request.post(options, function(err,httpResponse,body){ /* ... */ });
+      })
+    }
+    else{
+      User.danger = val;
+      console.log("normal")
+    }
+})
+firebase.database().ref(User.id+"/alarm").on('value', (snapshot)=>{
+  //console.log(snapshot.val());
+    val = snapshot.val();
+    if(val == 1){//danger!!
+      User.alarm = val;
+      console.log("alarm on!!!!!!");
+
+    }
+    else{
+      User.alarm = val;
+      console.log("alarm off")
+
+    }
+})
