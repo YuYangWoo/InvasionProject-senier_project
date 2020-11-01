89816801c3e0f1169f80f974ed6b89c225fbb798
yuyw0712 <yuyw0712@naver.com>
2020-11-01 20:56:28 +0900

    알람개수 안정화 코드 리팩토링

--- Invasion/app/src/main/java/com/cookandroid/invasion/MainActivity.kt
+++ Invasion/app/src/main/java/com/cookandroid/invasion/MainActivity.kt
@@ -59,7 +59,7 @@ class MainActivity : AppCompatActivity() {
                     val log = snapshot.getValue(LogItem::class.java) // 만들어뒀던 객체에 데이터를 담는다.
                     logList.add(log!!) // 담은 데이터들을 배열리스트에 넣고 리사이클러뷰로 보낼 준비
                 }
-                txtNumber.text = logList.size.toString()
+                txtNumber.append(logList.size.toString())
             }
 
             // 디비를 가져오던중 에러 발생 시 에러문 출력
